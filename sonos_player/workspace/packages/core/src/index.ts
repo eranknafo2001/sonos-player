5a6c0f9595f882dc46a4c7a6e5a7aa4424b04c76
Eran Knafo <eranknafo2001@gmail.com>
2026-04-03 20:15:14 +0300

    Remove generated HA publish artifacts from source repo

--- sonos_player/workspace/packages/core/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { app, AppService } from "./app/service";
-export type { AppSnapshot } from "./app/types";
-export { mqttState, MqttStateService } from "./mqtt/service";
-export * from "./sonos";
